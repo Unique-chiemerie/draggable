5e162e955405810b081a44fd29ba546c12316351
polymath-ic <luggyballz@gmail.com>
2023-09-26 01:04:20 +0100

    changing the whole build

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,60 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: const drag(),
+      home: const Drag(),
     ),
   );
 }
 
-// ignore: camel_case_types
-class drag extends StatefulWidget {
-  const drag({super.key});
+class Drag extends StatefulWidget {
+  const Drag({super.key});
+
   @override
-  State<drag> createState() => _dragState();
+  State<Drag> createState() => _DragState();
 }
 
-class _dragState extends State<drag> {
+class _DragState extends State<Drag> {
+  double left = 0;
+  double top = 0;
+
   @override
   Widget build(BuildContext context) {
-    double left = 0;
-    double top = 0;
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 210, 150, 220),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            Text(
+      backgroundColor: const Color.fromARGB(255, 210, 150, 220),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 70,
+          ),
+          Center(
+            child: Text(
               'Drag test',
               style: GoogleFonts.baloo2(fontSize: 30, color: Colors.white),
             ),
-            const SizedBox(
-              height: 90,
-            ),
-            Draggable(
-              feedback: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              //ondrag start
-
-              onDragUpdate: (details) {
-                setState(() {
-                  top += details.delta.dy;
-                  left += details.delta.dx;
-                });
-              },
-              child: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(10),
-                ),
+          ),
+          const SizedBox(
+            height: 90,
+          ),
+          GestureDetector(
+            onPanUpdate: (details) {
+              setState(() {
+                top = (top + details.delta.dy).clamp(0.0, double.maxFinite);
+                left = (left + details.delta.dx).clamp(0.0, double.maxFinite);
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.only(left: left, top: top),
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
